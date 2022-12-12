8d9e0d6de864bb2a661b89cb203f3404673deec8
Herman Kulik <herman_kulik@epam.com>
2022-12-12 21:20:48 +0100

    close #15, tie controller and service for vets

--- pet-clinic-web/src/main/java/guru/springframework/gurupetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/gurupetclinic/controllers/VetController.java
@@ -1,6 +1,9 @@
 package guru.springframework.gurupetclinic.controllers;
 
+import guru.springframework.gurupetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -8,9 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
 
     @RequestMapping({"/vets", "/vets/", "/vets/index", "vets/index.html"}) // endpoints are mapped to the method
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
